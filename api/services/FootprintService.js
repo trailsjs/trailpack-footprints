74b3b5dbbf7238de7785008706332a61421b67ff
Travis Webb <me@traviswebb.com>
2016-01-26 03:32:45 -0500

    [yo] generate trails:service FootprintController

---
+++ api/services/FootprintService.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const Service = require('trails-service')
+
+/**
+ * @module FootprintService
+ * @description The FootprintService transforms and forwards queries to the DatastoreService
+ */
+module.exports = class FootprintService extends Service {
+
+}
+
