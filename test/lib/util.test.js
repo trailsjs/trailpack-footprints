e21115ac5ef53c1c5093db11b9857fcdc155b361
Travis Webb <me@traviswebb.com>
2016-02-07 22:14:05 -0500

    [tests] include tests, fix getControllerHandler bug

---
+++ test/lib/util.test.js
@@ -0,0 +1,21 @@
+'use strict'
+
+//const _ = require('lodash')
+const assert = require('assert')
+const lib = require('../../lib')
+
+describe('lib.Util', () => {
+  describe('#getHandlerName', () => {
+    it('should return the handler name in a route handler', () => {
+      assert.equal(lib.Util.getHandlerName('TestController.testHandler'), 'testHandler')
+    })
+  })
+
+  describe('#getControllerHandler', () => {
+    it('should correctly combine controller and handler', () => {
+      assert.equal(lib.Util.getControllerHandler('TestController', 'testHandler'), 'TestController.testHandler')
+    })
+  })
+
+})
+
