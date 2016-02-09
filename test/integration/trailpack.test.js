47000c6335e1fc732c92512884b754c9043836d6
Travis Webb <me@traviswebb.com>
2016-02-08 19:26:06 -0500

    [test] test route prerequisites

--- test/integration/trailpack.test.js
+++ test/integration/trailpack.test.js
@@ -6,12 +6,20 @@ const assert = require('assert')
 describe('Router Trailpack', () => {
 
   describe('#initialize', () => {
-
     it('should include footprint routes in app.routes', () => {
       const routes = global.app.routes
 
       assert.equal(routes.length, 1)
+    })
+    it('should bind route handler to controller method', () => {
+      const routes = global.app.routes
+
       assert(_.isFunction(routes[0].handler))
+    })
+    it('should attach prerequisite methods', () => {
+      const routes = global.app.routes
+
+      assert(_.isFunction(routes[0].config.pre[0]))
     })
   })
 })
